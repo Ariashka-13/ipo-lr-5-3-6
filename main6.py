a9571304db946b813470150cbff11e3034b5e745
Ariashka <ariana0tsarova@gmail.com>
2024-10-17 21:58:30 +0300

    Add files via upload

---
+++ main6.py
@@ -0,0 +1,5 @@
+with open("text.txt", "r") as myfile: #открыть файл для чтения
+    text=myfile.read() #чтение текстового файла
+with open("output.txt", 'w') as out_txt: #создать новый текстовый файл
+    out_txt.write(text) #запись текста из одного текствого файла в другой
+print("Добавлены уникальные слова в файл output.txt.", text) #вывод что добавлен текст
