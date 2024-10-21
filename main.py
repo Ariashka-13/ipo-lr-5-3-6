eb14622d1ef5dac27131e2df882ac98d8de35aa1
Ariashka <ariana0tsarova@gmail.com>
2024-10-21 18:02:58 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#вариант3
 with open("text.txt", "r") as myfile: #открыть файл для чтения
-    text=myfile.read() #чтение текстового файла
+    text = myfile.read() #чтение текстового файла
 with open("output.txt", 'w') as out_txt: #создать новый текстовый файл
     out_txt.write(text) #запись текста из одного текствого файла в другой
 print("Добавлены уникальные слова в файл output.txt.", text) #вывод что добавлен текст
