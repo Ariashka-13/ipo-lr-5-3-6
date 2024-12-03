8dd00ab672e5885ec11a1cdfb639d6e28a73a305
Ariashka <ariana0tsarova@gmail.com>
2024-12-03 12:29:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 with open("text.txt", "r") as myfile: #открыть файл для чтения
     text = myfile.read() #чтение текстового файла
-with open("output.txt", 'w') as out_txt: #создать новый текстовый файл
-    out_txt.write(text) #запись текста из одного текствого файла в другой
-print("Добавлены уникальные слова в файл output.txt.", text) #вывод что добавлен текст
+unique = [] #создание пустого списка для уникальных слов
+words = text.split() #разделяем на слова
+for word in words: #проход по каждому слову
+    if word not in unique: #если нет повтор слова
+        unique.append(word) #добавление
+
+with open("output.txt", "w") as file: #открывать файл output.txt
+    for word in unique: #разделяем на слова
+        file.write(word + ' ') #записываем
